b0ef38850b7679e594b9e643cfaeeebe99b35fbf
nayadelacruz <del16001@byui.edu>
2024-09-17 14:45:38 -0700

    CReated code to assig grade to grade score

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your Grade Score? ");
+        string gradeScore = Console.ReadLine();
+        int numberGrade = int.Parse(gradeScore);
+
+        string letter = "";
+
+        if (numberGrade >= 90)
+        {
+            letter = "A";
+        }
+        else if (numberGrade >= 80)
+        {
+            letter = "B";
+        }
+        else if (numberGrade >= 70)
+        {
+            letter = "C";
+        }
+        else if (numberGrade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+
+
+        Console.WriteLine($"Your grade is: {letter}");
+
+        if (numberGrade >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time!");
+        }
     }
 }
