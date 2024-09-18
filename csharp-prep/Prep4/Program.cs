8b36ca8033a6d526e524005c4afe6736efa448e7
nayadelacruz <del16001@byui.edu>
2024-09-18 14:13:20 -0700

    created code to use list

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,59 @@
 using System;
-
+using System.Collections.Generic;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        //Enter a list of numbers, type 0 when finished.
+        //   Enter number: 18
+        //   Enter number: 36
+        //   Enter number: 2
+        //   Enter number: 48
+        //   Enter number: 6
+        //   Enter number: 12
+        //   Enter number: 9
+        //   Enter number: 0 
+
+        List<int> numbers = new List<int>();
+
+        int userNumber = -1;
+        while (userNumber != 0)
+        {
+            Console.Write("Enter a number (0 to quit): ");
+            string answer = Console.ReadLine();
+            userNumber = int.Parse(answer);
+
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+        }
+
+        // sum the numbers of the List
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+
+        // compute the average
+        float average = ((float)sum) / numbers.Count;
+
+        Console.WriteLine($"The average number is: {average}");
+        // compute the largest number
+        int largest = 0;
+
+        foreach (int number in numbers)
+        {
+            if (number > largest)
+            {
+                largest = number;
+            }
+        }
+
+        Console.WriteLine($"The largest number is: {largest}");
+
     }
 }
